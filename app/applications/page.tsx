e0f185f6b6e812c861b0895ec01411e39750fc11
mckenzie-SA <mckenziefoster89@gmail.com>
2026-06-06 22:47:19 +0100

    Add new page routes: login, dashboard, applications, flights, admin

---
+++ app/applications/page.tsx
@@ -0,0 +1,7 @@
+export default function Applications() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <h1 className="text-4xl font-bold">Applications</h1>
+    </main>
+  );
+}
